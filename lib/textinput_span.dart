ec0e19a12b539f5fa240b62996043a0569bac0a0
thong-dipro <thongtd@dipro.vn>
2023-07-17 05:28:53 +0700

    init project :tada:

---
+++ lib/textinput_span.dart
@@ -0,0 +1,11 @@
+library textinput_span;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
+
+void main() {
+  print("Hello World");
+}
